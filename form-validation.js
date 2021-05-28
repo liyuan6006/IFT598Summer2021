d80184f68d706d2c7faad25744826fb4e979310e
liyuan6006Git <liyuan6006@gmail.com>
2021-05-28 08:57:13 -0700

    update doc and including my partners work

--- form-validation.js
+++ form-validation.js
@@ -4,38 +4,123 @@ $(function() {
   // Initialize form validation on the registration form.
   // It has the name attribute "registration"
   registerForm.validate({
+	  focusCleanup: true,
     // Specify validation rules
     rules: {
       // The key name on the left side is the name attribute
       // of an input field. Validation rules are defined
       // on the right side
-      firstname: "required",
-      lastname: "required",
-      email: {
-        required: true,
-        // Specify that email should be validated
-        // by the built-in "email" rule
-        email: true
-      },
-      password: {
-        required: true,
-        minlength: 5
-      }
-    },
+			username: {
+				required: true,
+				rangelength: [2, 8]
+			},
+			psw: {
+				required: true,
+				maxlength: 8,
+				minlength: 8
+			},
+			pswrepeat: {
+				required: true,
+				equalTo: "#psw",
+				maxlength: 8,
+				minlength: 8	
+			},
+			lastname: {
+				required: true
+			},
+			gender: {
+				required: true 
+			},
+			streetaddress: {
+				required: true,
+			},
+			city: {
+				required: true 
+			},
+			state: {
+				required: true
+			},
+			zipcode: {
+				required: true,
+				digits: true,
+				minlength: 5,
+				maxlength: 5
+			},
+			email: {
+				required: true,
+				email: true 
+			},
+			cellphonenumber: {
+				required: true, 
+				phoneUS: true
+			},
+			origincountry: {
+				required: true
+			},
+			dob: {
+				required: true 
+			}
+		},
     // Specify validation error messages
-    messages: {
-      firstname: "Please enter your firstname hhhhhhhhhhhhhhhhhhh",
-      lastname: "Please enter your lastname",
-      password: {
-        required: "Please provide a password",
-        minlength: "Your password must be at least 5 characters long"
-      },
-      email: "Please enter a valid email address"
-    },
+		messages: {
+			username: {
+				required: 'Username is required',
+				rangelength: 'Must be 2 to 8 characters'
+			},
+			gender: {
+				required: 'Please select your gender'
+			},
+			pswrepeat: {
+				required: 'Verify Password',
+				equalTo: 'Must match password'
+			},
+
+			zipcode: {
+				required: 'Please enter a valid zip code'
+			},
+			city: {
+				required: 'Required - auto-populated by zipcode'
+			},
+			state: {
+				required: 'US State Only! - auto-populated by zipcode'
+			}
+		},
     // Make sure the form is submitted to the destination defined
     // in the "action" attribute of the form when valid
     submitHandler: function(form) {
       form.submit();
     }
   });
+$(function(){
+	(".pr-password").passwordRequirements({
+		numCharacters: 8,
+		useLowercase: true,
+		useUppercase: true,
+		useNumbers: true,
+		useSpecial: false
+	})
+});
+$("#zipcode").blur(function() {
+	var city = $("#city");
+	var state = $("#state");
+	if (!city.val() && $(this).valid()) {
+		$.getJSON("http://www.geonames.org/postalCodeLookupJSON?&country=US&callback=?", {postalcode: this.value }, function(response) {
+			if (!city.val() && response && response.postalcodes.length && response.postalcodes[0].placeName) {
+				city.val(response.postalcodes[0].placeName);
+			}
+		})
+	}
+		if (!state.val() && $(this).valid()) {
+		$.getJSON("http://www.geonames.org/postalCodeLookupJSON?&country=US&callback=?", {postalcode: this.value }, function(response) {
+			if (!state.val() && response && response.postalcodes.length && response.postalcodes[0].adminName1) {
+				state.val(response.postalcodes[0].adminName1);
+			}
+		})
+	}
+});
+$(function(){
+	$(".datepicker").datepicker({
+		format: 'mm/dd/yyyy'
+	})
+});
 });
