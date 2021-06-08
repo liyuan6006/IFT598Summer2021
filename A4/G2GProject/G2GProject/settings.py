8620f724f03d7b457028b99e87f427750caaa87d
Yuan Li <liyuan6006@gmail.com>
2021-06-08 12:21:28 -0700

    switch from the default SQLite to MySQL

--- A4/G2GProject/G2GProject/settings.py
+++ A4/G2GProject/G2GProject/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'G2GProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'impact',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',#based on your settings
+        'USER': 'devuser',#based on your settings
+        'PASSWORD': 'DevUser123',#based on your settings
     }
 }
 
